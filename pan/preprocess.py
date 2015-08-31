909818980677085b3cf6cc0cad97da7754b17baf
Andreas Grivas <agrv@iit.demokritos.gr>
2015-08-31 09:07:55 +0300

    cleaned preprocessing functions

--- pan/preprocess.py
+++ pan/preprocess.py
@@ -1,73 +1,42 @@
-import os.path
 import regex as re
 import string
 from textblob import TextBlob
 from BeautifulSoup import BeautifulSoup
 from HTMLParser import HTMLParser
-from stan import NER
 from collections import Counter
 
-# globals
-textregex = re.compile(r'!\[CDATA\[(?P<text>.*?)\]', re.DOTALL | re.UNICODE)
-urlregex = re.compile(r'(?P<all>\s?(?P<url>(https?|ftp)://[^\s/$.?#].[^\s]*))')
-# urlregex.sub('', text)
-hashregex = re.compile(r'(?<=\s+|^)(?P<all>#(?P<content>\w+))', re.UNICODE)
-# hashregex.sub('\g<2>', 'this #hashtag is cool #imo')
-replyregex = re.compile(r'(?P<all>(^|\s*)@(?P<content>\w+)\s*?)', re.UNICODE)
-# replyregex.sub('\g<2>', 'this #hashtag is cool #imo')
-viaregex = re.compile(r'(via *((@\w+\s?)|(https?://[^\s/$.?#].[^\s]*)))',
-                      re.UNICODE)
-saidregex = re.compile(r'((^| *|") *@\w+ *:)', re.UNICODE)
-quoteregex = re.compile(r'((?P<quote>"?.*"?) *[:\n\-.](?P<namedentity>.*))',
-                        re.UNICODE)
-
-# utility methods
-
-stanford_jar = os.path.abspath('resources/stanford-ner-2014-06-16/'
-                               'stanford-ner-3.4.jar')
-stanford_model = os.path.abspath('resources/stanford-ner-2014-06-16/'
-                                 'classifiers/english.all.3class.distsim'
-                                 '.crf.ser.gz')
-ner = None
+# regexps to capture hashtags - replies and urls
+URLREGEX = re.compile(r'(?P<all>\s?(?P<url>(https?|ftp)://[^\s/$.?#].[^\s]*))')
+HASHREGEX = re.compile(r'(?<=\s+|^)(?P<all>#(?P<content>\w+))', re.UNICODE)
+REPLYREGEX = re.compile(r'(?P<all>(^|\s*)@(?P<content>\w+)\s*?)', re.UNICODE)
 
 
-def quote_found(text):
-    """check if current text is a quotation - use ner
+# used to strip html from text
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
 
-    :text: TODO
-    :returns: TODO
+    def handle_data(self, d):
+        self.fed.append(d)
 
-    """
-    global ner
-    if not ner:
-        ner = NER(stanford_jar, stanford_model)
-    for match in quoteregex.finditer(text):
-        candidate = match.group('namedentity')
-        sent_ents = ner.tag(candidate)
-        for ents in sent_ents:
-            if 'PERSON' in ents:
-                return True
-    return False
-
-# TODO add quote - NER
-# add other quote styles
-quoteregexs = [viaregex.findall,
-               saidregex.findall,
-               quote_found
-               ]
+    def get_data(self):
+        return ''.join(self.fed)
 
 
-def get_docs(text):
-    """ Return documents from pan xml style data
+# --------------------- helper methods --------------------------
+
+def get_docs(xml):
+    """ Return documents from pan xml style data (pan 2014 dataset)
         documents are in <document> tags
 
-    :text: The xml as text
+    :xml: str - content of file to clean from xml
     :returns: list - list of str - documents
 
     """
-    bs = BeautifulSoup(text)
+    bs = BeautifulSoup(xml)
     # remove right tabs - why do you add tabs in CDATA?
-    docs = [doc.text.rstrip('\t') for doc in bs.findAll('document')]
+    docs = [doc.xml.rstrip('\t') for doc in bs.findAll('document')]
     return docs
 
 
@@ -77,20 +46,14 @@ def strip_tags(html):
     :text: The text with html to be removed
     :returns: str - the text
     """
-    class MLStripper(HTMLParser):
-        def __init__(self):
-            self.reset()
-            self.fed = []
-
-        def handle_data(self, d):
-            self.fed.append(d)
-
-        def get_data(self):
-            return ''.join(self.fed)
     s = MLStripper()
     s.feed(html)
     return s.get_data()
 
+
+# --------------------- preprocessing methods --------------------------
+# all these methods modify the data in place. They expect a mutable iterable
+# containing the texts as an input
 
 def clean_html(texts):
     """ Strip html
@@ -110,30 +73,15 @@ def detwittify(texts):
     """ remove characteristics which aren't author dependent like hashes and urls
 
     :texts: collection - the iterable of collection to change
-    :returns: nothing - modifies collection in place
-
     """
     for index, each in enumerate(texts):
         # remove urls from text
-        texts[index] = urlregex.sub('', each)
+        texts[index] = URLREGEX.sub('', each)
         # replace hashtags with the text of the tag
-        texts[index] = hashregex.sub('\g<2>', texts[index])
+        texts[index] = HASHREGEX.sub('\g<2>', texts[index])
         # replace @ tags
-        texts[index] = replyregex.sub('', texts[index])
+        texts[index] = REPLYREGEX.sub('', texts[index])
         texts[index] = texts[index].strip()
-
-
-def clear_quotations(texts):
-    """ Remove tweets that contain quotations
-    :returns: number - a count of how many quotations where removed
-
-    """
-    for index, text in reversed(list(enumerate(texts))):
-        for func in quoteregexs:
-            matches = func(text)
-            if matches:
-                del texts[index]
-                break
 
 
 def filter_lang(texts, lang):
@@ -141,9 +89,6 @@ def filter_lang(texts, lang):
 
     :texts: A list of texts to process
     :lang: The language we want to retain texts for
-    :returns: Nothing, modifies list in place retaining only texts
-              identified as written in the language we want
-
     """
     for index, text in reversed(list(enumerate(texts))):
         if len(text) > 3:
@@ -153,38 +98,17 @@ def filter_lang(texts, lang):
 
 
 def collapse_repetitions(texts):
-    """ Replace reptitions with 2 chars
-    :returns: TODO
-
+    """ Chop longer repetitions of characters to two
+        example: Hiiiii -> Hii
+        The rationale behind this is that people using different
+        lengths of repetition will be captured in the same way
+        (less features)
     """
     for index, each in enumerate(texts):
         # remove urls from text
         texts[index] = re.sub(r'([\w%s])\1+' % string.punctuation,
                               r'\1\1',
                               texts[index])
-
-
-def collapse_entities(texts):
-    """ collapse entities to a single character
-    :returns: TODO
-
-    """
-    global ner
-    if not ner:
-        ner = NER(stanford_jar, stanford_model)
-    for index, text in enumerate(texts):
-        ent_spans = ner.entity_spans(text)
-        # want this to be fast - many joins
-        string = ''
-        current = 0
-        for begin, end, tag in ent_spans:
-            string += text[current:begin]
-            # string += tag[0]
-            current = end
-        if current:
-            string += text[current:]
-            # we found some entities
-            texts[index] = string
 
 
 def remove_duplicates(texts):
@@ -203,7 +127,7 @@ def silhouette(texts):
        Replace lowercase letters with l
        Replace repetitions of characters with r
        Replace digits with d
-    :returns: TODO
+    :returns: Text converted to an outline form
 
     """
     for index, each in enumerate(texts):
